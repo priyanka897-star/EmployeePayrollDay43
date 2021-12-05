91b0a0d90549ddd612957d7414d6f1edf48a36df
priyanka897-star <priyankamore8997@gmail.com>
2021-12-05 21:11:45 +0530

    Added:Functionality to Display employee details to local Storage

--- Employee_Payroll_service/js/Home.js
+++ Employee_Payroll_service/js/Home.js
@@ -1,13 +1,24 @@
+let empPayrollList;
 window.addEventListener('DOMContentLoaded', (event) => {
+    empPayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
+    localStorage.removeItem('editEmp');
 });
-//Template litrals Es6 Features.
+    
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ?
+                        JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
+
+//Template litrals ES6 Features.
 const createInnerHtml = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>";
-let innerHtml = `${headerHtml}`;
-    let empPayrollList = createEmployeePayrollJSON();
+    if(empPayrollList.length == 0) return;
+    let innerHtml = `${headerHtml}`;
+   // let empPayrollList = createEmployeePayrollJSON();
     for(const empPayrollData of empPayrollList){
-        innerHtml =   `${innerHtml}
+        innerHtml = `${innerHtml}
             <tr>
                 <td><img class="profile" alt="" src="${empPayrollData._profilePic}"</td>
                 <td>${empPayrollData._name}</td>
@@ -74,7 +85,7 @@ const createEmployeePayrollJSON = () => {
             _id: new Date().getTime() + 1,
             _profilePic: '../assets/profile-images/Ellipse -4.png'
         }
-        
+
     ];
     return empPayrollListLocal;
 }
